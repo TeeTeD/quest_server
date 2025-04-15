26563bddcb4632b7bed76089746527c09191dad5
TeeTeD <120646483+TeeTeD@users.noreply.github.com>
2025-04-16 01:08:35 +0300

    Update entrypoint.sh

--- bookcase_latest/entrypoint.sh
+++ bookcase_latest/entrypoint.sh
@@ -8,16 +8,4 @@ check_internet() {
     fi
 }
 
-if check_internet; then
-    echo "Интернет доступен, клонируем репозиторий..."
-    git clone https://github.com/TeeTeD/quest_server.git /from_git
-    cd /from_git/bookcase_latest
-    echo "Использую версию от: $(find . -name "*.txt")"
-    exec python3 tcp8800a.py  
-else
-    echo "Интернет не доступен, использую последнюю локальную копию..."
-    cd /mnt/bookcase_latest
-
-    echo "Последняя локальная копия от: $(find . -name "*.txt")"
-    exec python3 tcp8800a.py  
-fi
+exec python3 tcp8800a.py  
