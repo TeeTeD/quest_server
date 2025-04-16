31730eaa4a834b036470fd60c03fc92dcd6f8680
TeeTeD <120646483+TeeTeD@users.noreply.github.com>
2025-04-16 17:53:20 +0300

    Update tcp8800a.py

--- bookcase_latest/tcp8800a.py
+++ bookcase_latest/tcp8800a.py
@@ -1,4 +1,5 @@
 import asyncio
+import time
 from datetime import datetime, timedelta
 
 devices = {}
@@ -205,11 +206,11 @@ async def bookcase_handler():
                         book_1_press = True
                         if not book_2_flag and not book_3_flag and not already_wrong:
                             book_1_flag = True
-                            writer.write(LC13(0, 58, 10, 0, 255, 0, 200).encode('utf-8'))
+                            writer.write(LC13(0, 60, 6, 0, 255, 0, 500).encode('utf-8'))
                             await writer.drain()
                         else:
                             already_wrong = True
-                            writer.write(LC13(0, 58, 10, 255, 0, 0, 200).encode('utf-8'))
+                            writer.write(LC13(0, 60, 6, 255, 0, 0, 500).encode('utf-8'))
                             await writer.drain()
 
                     
@@ -217,11 +218,11 @@ async def bookcase_handler():
                         book_2_press = True
                         if book_1_flag and not book_3_flag and not already_wrong:
                             book_2_flag = True
-                            writer.write(LC13(0, 39, 10, 0, 255, 0, 200).encode('utf-8'))
+                            writer.write(LC13(0, 41, 6, 0, 255, 0, 500).encode('utf-8'))
                             await writer.drain()
                         else:
                             already_wrong = True
-                            writer.write(LC13(0, 39, 10, 255, 0, 0, 200).encode('utf-8'))
+                            writer.write(LC13(0, 41, 6, 255, 0, 0, 500).encode('utf-8'))
                             await writer.drain()
 
 
@@ -229,31 +230,32 @@ async def bookcase_handler():
                         book_3_press = True
                         if book_1_flag and book_2_flag and not already_wrong:
                             book_3_flag = True
-                            writer.write(LC13(0, 7, 10, 0, 255, 0, 200).encode('utf-8'))
+                            writer.write(LC13(0, 0, 74, 0, 255, 0, 5000).encode('utf-8'))
+                            time.sleep(5)
                             writer.write(SETD(4, True).encode('utf-8'))
                             await writer.drain()
                         else:
                             already_wrong = True
-                            writer.write(LC13(0, 7, 10, 255, 0, 0, 200).encode('utf-8'))
+                            writer.write(LC13(0, 9, 6, 255, 0, 0, 500).encode('utf-8'))
                             await writer.drain()
                         
 
                     if "GETD220" in packet:
                         book_1_press = False
                         book_1_flag = False
-                        writer.write(LC13(0, 58, 10, 0, 0, 0, 200).encode('utf-8'))
+                        writer.write(LC13(0, 60, 6, 0, 0, 0, 500).encode('utf-8'))
                         await writer.drain()
 
                     if "GETD230" in packet:
                         book_2_press = False
                         book_2_flag = False
-                        writer.write(LC13(0, 39, 10, 0, 0, 0, 200).encode('utf-8'))
+                        writer.write(LC13(0, 41, 6, 0, 0, 0, 500).encode('utf-8'))
                         await writer.drain()
 
                     if "GETD270" in packet:
                         book_3_press = False
                         book_3_flag = False
-                        writer.write(LC13(0, 7, 10, 0, 0, 0, 200).encode('utf-8'))
+                        writer.write(LC13(0, 9, 6, 0, 0, 0, 500).encode('utf-8'))
                         await writer.drain()
                         
                     if not book_1_press and not book_2_press and not book_3_press:
