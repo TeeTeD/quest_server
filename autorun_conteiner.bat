91d068cbd11a5a05a52ce567a4a9410f25149080
TeeTeD <120646483+TeeTeD@users.noreply.github.com>
2025-05-04 15:25:10 +0300

    Update autorun_conteiner.bat

--- autorun_conteiner.bat
+++ autorun_conteiner.bat
@@ -26,4 +26,4 @@ cd bookcase_latest
 
 echo Starting conteiner...
 
-docker run -e PYTHONUNBUFFERED=1 -p 8800:8800 clean_ubuntu:v1
+docker run -v C:\server_stuff:<путь_в_контейнере> -e PYTHONUNBUFFERED=1 -p 8800:8800 clean_ubuntu:v1
