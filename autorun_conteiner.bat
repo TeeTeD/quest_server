2c4216ed491c8a9e6a79801f8e755fb0a7fc4bac
TeeTeD <120646483+TeeTeD@users.noreply.github.com>
2025-05-04 15:41:16 +0300

    Update autorun_conteiner.bat

--- autorun_conteiner.bat
+++ autorun_conteiner.bat
@@ -26,4 +26,4 @@ cd bookcase_latest
 
 echo Starting conteiner...
 
-docker run -v C:\server_stuff:/usr/local/bin/ -w /usr/local/bin/ -it python:3.13.3 python tcp8800a.py -e PYTHONUNBUFFERED=1 -p 8800:8800 clean_ubuntu:v1
+docker run -v C:\server_stuff:/usr/local/bin/ -w /usr/local/bin/bookcase_latest -it clean_ubuntu:v1 entrypoint.sh -e PYTHONUNBUFFERED=1 -p 8800:8800 clean_ubuntu:v1
