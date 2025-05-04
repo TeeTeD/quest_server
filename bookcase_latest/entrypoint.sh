d31cf5190f38efbba4141408d521e6855444efa4
TeeTeD <120646483+TeeTeD@users.noreply.github.com>
2025-05-04 15:33:59 +0300

    Update entrypoint.sh

--- bookcase_latest/entrypoint.sh
+++ bookcase_latest/entrypoint.sh
@@ -1,11 +1,1 @@
 #!/bin/bash
-
-check_internet() {
-    if curl -s --connect-timeout 5 https://github.com > /dev/null; then
-        return 0
-    else
-        return 1
-    fi
-}
-
-exec python3 /usr/local/bin/tcp8800a.py  
